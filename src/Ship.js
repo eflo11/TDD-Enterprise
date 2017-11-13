45f0f6156e4aa886760cbe512b9352594d63cc82
Flory, Ethan (ETW - FLEX) <ethan.flory@nike.com>
2017-11-13 10:54:01 -0800

    Add in missileLauncher

--- src/Ship.js
+++ src/Ship.js
@@ -5,8 +5,42 @@
  */
 class Ship {
 
-  constructor() {
+ constructor() {
 
+   this.hitpoints = 100;
+   this.submodules = {
+    missileLauncher: {
+      status: 'OK',
+      missilesLaunched: []
+    }
+  };
+}
+
+ // Method
+
+ nextRound() {
+
+  }
+
+ damageShip(dmg) {
+    this.hitpoints -= dmg;
+  }
+
+  damageSubmodule(submodule) {
+     switch(this.submodules[submodule].status) {
+       case 'OK':
+        this.submodules[submodule].status = 'DAMAGED';
+        break;
+      case 'DAMAGED':
+        this.submodules[submodule].status = 'DESTROYED';
+        break;
+      default:
+        break;
+     }
+  }
+
+  fireMissile(target) {
+    this.missileLauncher.missilesLaunched.push(target);
   }
 
 }
--- test/ShipSpec.js
+++ test/ShipSpec.js
@@ -9,18 +9,76 @@ let Ship = require('../src/Ship');
  */
 describe('Ship', () => {
 
-  let ship;
+ let ship;
 
-  beforeEach(() => {
+ beforeEach(() => {
     ship = new Ship();
   });
 
-  describe('Some ship feature', () => {
+ describe('Some ship feature', () => {
 
-    it('should do something', () => {
+   it('should do something', () => {
       // What should it do?
     });
 
+
+ });
+
+ describe('Round', () => {
+
+   it('should process a round', () => {
+      assert.isFunction(ship.nextRound, 'should be a method on ship');
+    });
+
+ });
+
+ describe('Hull', () => {
+
+   it('should have hitpoints', () => {
+      assert.isNumber(ship.hitpoints, 'should have hitpoints');
+      assert.strictEqual(ship.hitpoints, 100, 'should have initialized 100 hitpoints');
+    });
+
+   it('should be damageable', () => {
+      assert.isFunction(ship.damageShip, 'should be a method damageShip');
+      ship.damageShip(25);
+      assert.strictEqual(ship.hitpoints, 75, 'should have 75 hitpoints left');
+   });
   });
+
+ describe('SubModules - Foundation', () => {
+
+   describe('should have a SubModules property', () => {
+      it('should be an object', () => {
+        assert.isObject(ship.submodules);
+      });
+
+   });
+
+   it('should have a damageSubmodule function', () => {
+     assert.isFunction(ship.damageSubmodule, 'should be function');
+     assert.strictEqual(ship.submodules.missileLauncher.status, 'OK', 'should have an OK status');
+     ship.damageSubmodule('missileLauncher');
+     assert.strictEqual(ship.submodules.missileLauncher.status, 'DAMAGED', 'should have degraded status of submodule');
+     ship.damageSubmodule('missileLauncher');
+     assert.strictEqual(ship.submodules.missileLauncher.status, 'DESTROYED', 'submodule should have been DESTROYED');
+     ship.damageSubmodule('missileLauncher');
+     assert.strictEqual(ship.submodules.missileLauncher.status, 'DESTROYED', 'should remain at the DESTROYED state');
+   });
+
+   it('should have a submodule for missileLauncher', () => {
+     assert.isObject(ship.submodules.missileLauncher, 'should have a submodule for missileLauncher');
+     assert.strictEqual(ship.submodules.missileLauncher.status, 'OK', 'should have a status object on the missileLauncher');
+     assert.isArray(ship.submodules.missileLauncher.missilesLaunched, 'should be an array');
+   });
+
+   it('should fireMissile at target', () => {
+     assert.isFunction(ship.fireMissile, 'should have a fireMissile on ship');
+     ship.fireMissile({
+       
+     });
+   });
+
+ });
 
 });
